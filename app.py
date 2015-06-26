7aaa6a5303dfbb600eaabf9cd41ca1b9e7fbfaf3
nuruddeen <nuruddeensalihu@yahoo.com>
2015-06-26 13:20:52 +0800

    First Commit, Templates created, Database ready, Account module finished , an error is raised by login_manager

---
+++ app.py
@@ -0,0 +1,151 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+from sqlalchemy.orm import sessionmaker
+from models.models import Base,User,Product,ProductItem,DATABASE, initialize
+from sqlalchemy import exists
+from flask_wtf import Form
+from forms import RegisterForm
+from flask.ext.bcrypt import check_password_hash
+from flask.ext.login import LoginManager,login_user
+
+DEBUG  = True
+PORT = 8080
+HOST = '0.0.0.0'
+re_createTable = False
+ 
+
+
+engine = DATABASE
+Base.metadata.bind = (engine)
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+app = Flask(__name__)
+
+    
+@app.route('/',methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.form.get('login', None)  == 'Login' :
+        return authenticate()
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+@app.route('/collections')
+def collections():
+    return render_template('collections.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/createaccount', methods = ['GET','POST'])
+def createaccount():
+    form = RegisterForm.RegisterForm()
+    if request.method =='GET':
+        return render_template('createaccount.html',form=form)
+    if request.method =='POST':
+        if form.validate_on_submit():
+            flash("yay, you registered!", "success")
+            User.create_user(
+                title = form.title.data,
+                fname = form.fname.data,
+                lname = form.lname.data,
+                username = form.username.data,
+                email = form.email.data,
+                password = form.password.data,
+                address = form.address.data,
+                state = form.state.data,
+                is_Admin = True
+                )
+            return render_template('accountsuccess.html', email=request.form['email'])
+        return render_template('createaccount.html', form=form)
+
+@app.route('/lookbook')
+def lookbook():
+    return render_template('lookbook.html')
+
+
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    form = RegisterForm.LoginForm()
+    if request.method == 'GET':
+        return render_template('login.html', form=form)
+    return authenticate(form = form)
+    
+
+@app.before_request
+def before_request():
+     """Connect to the database connection before each request. """
+
+@app.after_request
+def after_request(response):
+    """Close database connection after each request. """
+    #g.db.close()
+    return response
+
+
+def authenticate(form):
+    if form.validate_on_submit():
+        try:
+            user = session.query(User).filter(User.email == form.email.data).first()
+        except :# models.DoesNotExist:
+            flash("Your email or password does not match !", "error")
+        else :
+            if check_password_hash(user.password,form.password.data):
+                login_user(user)
+                flash("You've been logged in", "success")
+                return redirect(url_for('index'))
+            else :
+                flash("Your email or password does not match !", "error")
+    return render_template('login.html',form = form)           
+
+if __name__ == '__main__':
+    initialize(re_createTable)
+    try:
+        User.create_user(
+            title='MR',
+            fname = 'Musa',
+            lname = 'Salihu',
+            username = 'Musa',
+            email = 'musa.3@hotmail.com',
+            password = 'password',
+            address = 'Block 46A-3-4 Siri Ixora Jalan Pjs Seksen 29/11 Shah Alam Malaysia',
+            state = 'Selangor',
+            is_Admin = True
+            )
+    except:
+        pass
+        
+    app.secret_key = 'Innalhamdulillah.nahmaduhu.taalanastainubihi.wanastagfiruh!'
+    app.run(debug = DEBUG, host=HOST, port= PORT)
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(userid):
+        try:
+            return session.query(User).filter(User.id == userid).first()
+        except models.DoesNotExist :
+            return None
---
+++ models/models.py
@@ -0,0 +1,122 @@
+import os
+import sys
+import psycopg2
+from datetime import datetime
+from flask.ext.login import UserMixin
+from flask.ext.bcrypt import generate_password_hash
+from sqlalchemy_utils import ArrowType
+import arrow
+from sqlalchemy import types
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Binary, CHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
+
+
+
+
+Base = declarative_base()
+
+engine = create_engine('postgresql://postgres:0102443167@localhost:5432/postgres',echo=True)
+Base.metadata.bind = (engine)
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+DATABASE = engine
+
+class User(UserMixin , Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    title = Column(CHAR(3), nullable = False)
+    fname = Column(String(100), nullable = False)
+    lname = Column(String(100), nullable = False)
+    username = Column(String(100), nullable = False, unique = True)
+    email = Column (String(50), nullable =False, unique = True)
+    password = Column(String(100), nullable = False)
+    address = Column(String(250), nullable = False)
+    state = Column(String(50), nullable = False)
+    is_Admin = Column(Boolean ,default = False)
+    is_Logged = Column(Boolean, default = False)
+    is_Active = Column (Boolean , default = False)
+    is_Block = Column(Boolean, default = False)
+    joined_On = Column(ArrowType)
+    
+
+    @classmethod
+    def create_user(self,fname, lname,username,email,password,address,state,title, is_Admin = False):
+        try:
+            session = DBSession()
+            myFirstUser = User(
+                title = title,
+                fname = fname,
+                lname = lname,
+                username = username,
+                email = email,
+                password = generate_password_hash(password),
+                address = address,
+                state = state,
+                is_Admin = is_Admin,
+                joined_On = arrow.utcnow())
+            
+          
+            session.add(myFirstUser)
+            session.commit()
+            
+        except IntegrityError :
+            # recreate the session and re-add your object
+            session = DBSession()
+            session.add(User)
+            raise ValueError('user Already Exist !')
+            
+        
+
+    def is_authenticated(self):
+        return True
+ 
+    def is_active(self):
+        return True
+ 
+    def is_anonymous(self):
+        return False
+ 
+    def get_id(self):
+        return unicode(self.id)
+ 
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+    
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key =True)
+    title = Column(String(250), nullable = False)
+    description = Column(String(500), nullable = False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User)
+
+class ProductItem(Base):
+    __tablename__ ='product_items'
+    id = Column(Integer , primary_key = True)
+    image_Type = Column(Integer , unique= False)
+    image_Size = Column(Integer, default = 0)
+    Image = Column(Binary, nullable =False)
+    description = Column(String(500), nullable = False)
+    product_id = Column(Integer, ForeignKey('products.id'))
+    product = relationship(Product)
+    
+
+def initialize(re_createTable= False):
+    #connection = DATABASE.connect()
+    
+    if re_createTable :
+        Base.metadata.drop_all(DATABASE, checkfirst = True)
+        
+    
+    Base.metadata.create_all(DATABASE, checkfirst = True)
+    #connection.close()
+    
+        
+            
+            
