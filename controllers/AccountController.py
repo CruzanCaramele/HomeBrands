e2eab21ceaf06e12b0ea86206453d9887be36411
nuruddeen <nuruddeensalihu@yahoo.com>
2015-06-28 20:24:27 +0800

    Code refactored,RegisterForm Changed to accountform, comments added, controller created

---
+++ controllers/AccountController.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,request,redirect,url_for,flash,g
+from sqlalchemy.orm import sessionmaker
+from models.models import Base,User,Product,ProductItem,DATABASE, initialize
+from sqlalchemy import exists
+from flask.ext.bcrypt import check_password_hash
+from flask.ext.login import LoginManager,login_user, logout_user,login_required,current_user
+from forms import AccountForm
+
+#Database engine settings imported from module models
+engine = DATABASE
+Base.metadata.bind = (engine)
+
+#DbSEssion from alchemy provide user with a database connection session.
+#Allowing queries and manipulation. 
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+#authenticate user
+def authenticate(form):
+    if form.validate_on_submit():
+        try:
+            user = session.query(User).filter(User.email == form.email.data).first()
+        except :# models.DoesNotExist:
+            flash("Your email or password does not match !", "error")
+        else :
+            if check_password_hash(user.password,form.password.data):
+                login_user(user, remember = form.remember.data)
+                flash("You've been logged in", "success")
+                return redirect(url_for('index'))
+            else :
+                flash("Your email or password does not match !", "error")
+                return render_template('login.html',form = form)
+    return render_template('login.html',form = form)       
+
+#Create Account
+def createuser(form):
+    if form.validate_on_submit():
+            flash("yay, you registered!", "success")
+            User.create_user(
+                title = form.title.data,
+                fname = form.fname.data,
+                lname = form.lname.data,
+                username = form.username.data,
+                email = form.email.data,
+                password = form.password.data,
+                address = form.address.data,
+                state = form.state.data,
+                is_Admin = True
+                )
+            return render_template('accountsuccess.html', email=request.form['email'])
+    return render_template('createaccount.html', form=form)
---
+++ forms/AccountForm.py
@@ -0,0 +1,48 @@
+from flask_wtf import Form
+from wtforms import StringField,PasswordField,TextAreaField,DateField,SelectField,RadioField,BooleanField
+from wtforms.validators import (DataRequired, Regexp, ValidationError,
+                                Email, Length, EqualTo)
+from models.models import Base,User,Product,ProductItem,DATABASE
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import exists
+
+
+engine = DATABASE
+Base.metadata.bind = (engine)
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+ 
+#Check if username exists
+def name_exists(form, field):
+    if session.query(exists().where(User.username == field.data)).scalar()  :
+        raise ValidationError('User with the username already exists !')
+
+#Check if email exists    
+def email_exists(form, field):
+    if session.query(exists().where(User.email == field.data)).scalar() :
+        raise ValidationError('User with the email already exists !')
+    
+class RegisterForm(Form):
+    title = RadioField('Title', choices =[('Mr','Mr'),('Ms','Ms')])
+    fname = StringField('First Name',validators = [ DataRequired() ])
+    lname = StringField( 'Last Name',validators = [DataRequired()])
+    username = StringField('Username',
+                           validators = [DataRequired(),
+                                         Regexp(r'^[a-zA-Z0-9_]+$',
+                                         message = ("Username should be one word , letters,"
+                                                    "numbers, and underscore only")),
+                                         name_exists])
+    email =  StringField('Email',validators = [DataRequired(),Email(),email_exists])
+    password = PasswordField('Password',validators = [DataRequired(),Length(min=8),
+                                        EqualTo('password2', message = 'Password does not match !')])
+    password2 = PasswordField('Re-Enter Password',validators = [DataRequired()])
+    address = TextAreaField('Address',validators = [DataRequired()])
+    state = SelectField('State', choices=[('us','USA'),('gb','Great Britain'),('ru','Russia')])
+                
+                
+class LoginForm(Form):
+    email = StringField('Enter email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Password')
